2b8a0d355a170466970129163f10e3091cd85398
e10tech <e10fortech@gmail.com>
2025-06-08 16:02:33 +0900

    output: 'standalone'を追加

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
       },
+      output: 'standalone', // ←これを追加！
 }
 
 module.exports = nextConfig
